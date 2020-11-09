a43c91b449c0cf8c829397bcfbf556d6d323144a
André Pereira <a.pereira@eyeo.com>
2020-11-09 14:09:04 +0000

    Backport benchmarking

---
+++ adblock-android-benchmark/src/androidTest/kotlin/org/adblockplus/libadblockplus/benchmark/FilterEngineLoadTimeBenchmark.kt
@@ -0,0 +1,217 @@
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-present eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.adblockplus.libadblockplus.benchmark
+
+import android.opengl.Matrix
+import android.os.SystemClock
+import androidx.benchmark.junit4.BenchmarkRule
+import androidx.benchmark.junit4.measureRepeated
+import androidx.test.platform.app.InstrumentationRegistry
+import org.adblockplus.libadblockplus.FileSystem
+import org.adblockplus.libadblockplus.FilterEngine
+import org.adblockplus.libadblockplus.Platform
+import org.adblockplus.libadblockplus.android.AdblockEngine
+import org.adblockplus.libadblockplus.android.AndroidHttpClient
+import org.adblockplus.libadblockplus.android.AndroidHttpClientResourceWrapper
+import org.adblockplus.libadblockplus.android.AndroidHttpClientResourceWrapper.Storage
+import org.adblockplus.libadblockplus.android.TimberLogSystem
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.Timeout
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+import java.io.File
+import java.util.concurrent.TimeUnit
+import kotlin.math.abs
+import kotlin.system.measureTimeMillis
+
+fun randomDirectory() = File.createTempFile("adblock", ".tmpdir").also {
+    it.delete()
+    it.mkdirs()
+}
+
+class FilterEngineLoadTimeBenchmark {
+    companion object {
+        // used for waiting of subscriptions ready
+        private const val SLEEP_INTERVAL_MILLIS = 1L
+
+        // Warning: should be in sync with main/res/raw/easylist.txt and exceptionrules.txt
+        private const val PATTERNS_INI_LENGTH = 365427L
+
+        private val context = InstrumentationRegistry.getInstrumentation().targetContext
+        private val resourceIdToResourceMap = mapOf(
+            // Warning: it's required to have all the possible values from ..Wrapper
+
+            // locale-specific
+            AndroidHttpClientResourceWrapper.EASYLIST to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_INDONESIAN to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_BULGARIAN to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_CHINESE to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_CZECH_SLOVAK to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_DUTCH to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_GERMAN to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_ISRAELI to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_ITALIAN to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_LITHUANIAN to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_LATVIAN to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_ARABIAN_FRENCH to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_FRENCH to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_ROMANIAN to R.raw.easylist,
+            AndroidHttpClientResourceWrapper.EASYLIST_RUSSIAN to R.raw.easylist,
+
+            // acceptable ads
+            AndroidHttpClientResourceWrapper.ACCEPTABLE_ADS to R.raw.exceptionrules
+        )
+    }
+
+    @get:Rule
+    val globalTimeout = Timeout(15, TimeUnit.MINUTES)
+
+    @get:Rule
+    val benchmarkRule = BenchmarkRule()
+
+    private class Setup(basePath: String) {
+        val logSystem = TimberLogSystem()
+        val fileSystem: FileSystem? = null // default C++ impl
+        val wrapperStorage = object : Storage {
+            override fun put(url: String?) { /* nothing */ }
+            override fun contains(url: String?) = false // we don't need downloading at all
+        }
+
+        // we want to isolate the changes and remove the influence of network speed.
+        // so providing subscriptions content from test resources instead of actual downloading
+        val httpClientWrapper = AndroidHttpClientResourceWrapper(
+            context, AndroidHttpClient(), resourceIdToResourceMap, wrapperStorage)
+
+        // we need to randomize basePath to avoid test interference (shared state from persistence)
+        val platform = Platform(logSystem, fileSystem, httpClientWrapper, basePath)
+
+        init {
+            httpClientWrapper.setListener {
+                url, _ -> Timber.i("Intercepted $url to avoid networking")
+            }
+            platform.setUpJsEngine(AdblockEngine.generateAppInfo(context, true))
+            Timber.d("Path = $basePath")
+        }
+    }
+
+    private fun waitForSubscriptionsLoaded(filterEngine: FilterEngine) {
+        Timber.d("Waiting for ready subscriptions ...")
+        val subscriptionsReadyMillis = measureTimeMillis {
+            var subscriptions = filterEngine.listedSubscriptions
+            Timber.d("Current subscriptions: $subscriptions")
+            while (subscriptions.size != 2 || // 2 = locale-specific + AA
+                !subscriptions.all {
+                    it.getProperty("downloadStatus")?.asString() == "synchronize_ok"
+                }) {
+                SystemClock.sleep(SLEEP_INTERVAL_MILLIS)
+                subscriptions = filterEngine.listedSubscriptions
+                Timber.d("Current subscriptions: $subscriptions")
+            }
+        }
+        Timber.d("Subscriptions ready in $subscriptionsReadyMillis millis")
+    }
+
+    private fun waitForSubscriptionsSaved(pattersIniFile: File) {
+        while (!pattersIniFile.exists() || pattersIniFile.length() < PATTERNS_INI_LENGTH) {
+            SystemClock.sleep(SLEEP_INTERVAL_MILLIS)
+        }
+    }
+
+    private inline fun getPatternsIniFile(dir: File) = File(dir, "patterns.ini")
+
+    private fun createAndDispose(path: String, onCreated: ((FilterEngine) -> Unit)? = null) {
+        val setup = Setup(path)
+        with(setup.platform) {
+            Timber.d("Creating FilterEngine ...")
+            lateinit var filterEngine: FilterEngine
+            try {
+                val loadTime = measureTimeMillis {
+                    filterEngine = getFilterEngine()
+                }
+                Timber.d("Created in $loadTime millis")
+                if (onCreated != null) {
+                    onCreated(filterEngine)
+                }
+            } finally {
+                dispose()
+            }
+        }
+    }
+
+    private inline fun test(dir: File, waitForSaved: Boolean) {
+        createAndDispose(dir.absolutePath) {
+            waitForSubscriptionsLoaded(it)
+            if (waitForSaved) {
+                waitForSubscriptionsSaved(getPatternsIniFile(dir))
+            }
+        }
+    }
+
+    // an example for proper benchmarking sample, taken from:
+    // https://cs.android.com/androidx/platform/frameworks/support/+/androidx-master-dev:benchmark/common/src/main/java/androidx/benchmark/ThrottleDetector.kt;l=47;drc=18266f9efb44e8e63a45e055ffd471dd11544c02
+    @Test
+    fun measurePureCalculation() {
+        val sourceMatrix = FloatArray(16) { System.nanoTime().toFloat() }
+        val resultMatrix = FloatArray(16)
+
+        benchmarkRule.measureRepeated {
+            repeat(1000) {
+                Matrix.translateM(resultMatrix, 0, sourceMatrix, 0, 1F, 2F, 3F)
+            }
+        }
+    }
+
+    @Test
+    fun measureVeryFirstTime() {
+        // "very first time" means the subscriptions data is going to be downloaded (actually loaded
+        // from app resources), parsed and saved
+
+        benchmarkRule.measureRepeated {
+            test(randomDirectory(), false)
+        }
+    }
+
+    @Test
+    fun measureNotTheFirstTime() {
+        // "not the first time" means subscriptions data is already downloaded and saved on SD
+
+        val dir = randomDirectory()
+        val pattersIniFile = getPatternsIniFile(dir)
+
+        // download, wait for subscriptions to be ready and saved
+        test(dir, true)
+
+        // check saved subscriptions data
+        assertTrue(pattersIniFile.exists())
+
+        // Note: the actual file size can be a bit different when building locally vs. on CI
+        // due to different datetimes (lastDownload, lastSuccess) and some empty lines
+        assertTrue(abs(pattersIniFile.length() - PATTERNS_INI_LENGTH) < 10)
+        Timber.d("State is ready, starting benchmarking")
+
+        // the dir is the same as it was used for the first FE create,
+        // so it will read the subscriptions data from the persistent storage
+        benchmarkRule.measureRepeated {
+            test(dir, false)
+        }
+    }
+}
---
+++ tools/memory_benchmark.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+"""
+This script runs memory benchmark on android device.
+1) installs apk for testing assuming it already is built
+2) iterates following instructions NUM_TESTS times:
+2.1) runs each test scenario under `memory_benchmark_test` array
+2.2) collects the .csv output.
+3) calculates average for the test scenarios and inserts it into a metrics.txt
+   file
+"""
+
+import argparse
+import csv
+import subprocess
+
+memory_benchmark_test = ["MemoryBenchmark_full_easy", "MemoryBenchmark_minDist"]
+
+TEST_PACKAGE = "org.adblockplus.libadblockplus.android.webview.test"
+NUM_TESTS = 10
+OUTPUT_LOCATION = "adblock-android-webview/build/outputs/memory_benchmark"
+COOL_OFF_ROW = 5
+TOTAL_ANDROID_STUDIO_COLUMN = 2
+
+
+def exit_with(message):
+    print(message)
+    sys.exit(1)
+
+
+def run_command(command, exitOnError=True):
+    child = subprocess.Popen(command, shell=True, close_fds=True)
+    child.communicate()
+    if child.returncode != 0:
+        if exitOnError:
+            exit_with(command + " failed!")
+        else:
+            print(command + " failed!")
+
+
+def get_total_memory(memory_benchmark_csv):
+    with open("{}/{}".format(OUTPUT_LOCATION, memory_benchmark_csv), mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        line_count = 0
+        minDist_min_AA = 0
+
+        for row in csv_reader:
+            if line_count == COOL_OFF_ROW:
+                minDist_min_AA = row[TOTAL_ANDROID_STUDIO_COLUMN]
+            line_count += 1
+        return minDist_min_AA
+
+
+def main(args):
+    adb_device = "adb"
+    if args.device is not None:
+        adb_device = "adb -s {}".format(args.device)
+
+    run_command("{} logcat -c".format(adb_device))
+
+    print("Preparing to run benchmark tests for: {}".format(TEST_PACKAGE))
+
+    run_command("{} uninstall {}".format(adb_device, TEST_PACKAGE), False)
+    run_command("{} install -r \
+      adblock-android-webview/build/outputs/apk/androidTest/release/adblock-android-webview-release-androidTest.apk"
+                .format(adb_device))
+
+    run_command("{} shell pm grant {} android.permission.READ_EXTERNAL_STORAGE".format(adb_device,
+                                                                                       TEST_PACKAGE))
+    run_command("{} shell pm grant {} android.permission.WRITE_EXTERNAL_STORAGE".format(adb_device,
+                                                                                        TEST_PACKAGE))
+
+    print("Running memory benchmark tests...")
+
+    minDist_min_AA_sum = 0
+    full_easy_min_AA_sum = 0
+
+    for i in range(0, NUM_TESTS):
+        run_command("mkdir -p {}".format(OUTPUT_LOCATION))
+        run_command("rm -f {}/*".format(OUTPUT_LOCATION))
+        for test in memory_benchmark_test:
+            print("Running {}...".format(test))
+            print("Testing with AA enabled")
+            run_command("{} shell am instrument -w -r -e debug false -e class {}.{}_full_AA -e \
+            annotation androidx.test.filters.LargeTest {}/androidx.test.runner.AndroidJUnitRunner"
+                    .format(adb_device, TEST_PACKAGE, test, TEST_PACKAGE))
+
+            print("Test done pulling results with AA enabled")
+            run_command("{} pull /storage/emulated/0/Download/memory_benchmark.csv {}/{}_full_AA.csv"
+                    .format(adb_device, OUTPUT_LOCATION, test))
+
+            print("Testing without AA...")
+            run_command("{} shell am instrument -w -r -e debug false -e class {}.{}_min_AA -e \
+            annotation androidx.test.filters.LargeTest {}/androidx.test.runner.AndroidJUnitRunner"
+                    .format(adb_device, TEST_PACKAGE, test, TEST_PACKAGE))
+
+            print("Test done pulling results without AA enabled")
+            run_command("{} pull /storage/emulated/0/Download/memory_benchmark.csv {}/{}_min_AA.csv"
+                    .format(adb_device, OUTPUT_LOCATION, test))
+
+        minDist_min_AA_sum += int(get_total_memory("MemoryBenchmark_minDist_min_AA.csv"))
+        full_easy_min_AA_sum += int(get_total_memory("MemoryBenchmark_full_easy_min_AA.csv"))
+
+    run_command("{} logcat -d > {}/logcat.log".format(adb_device, OUTPUT_LOCATION))
+    run_command("{} logcat -d -b main > {}/logcat_main.log".format(adb_device, OUTPUT_LOCATION))
+    run_command("{} logcat -d -b crash > {}/logcat_crash.log".format(adb_device, OUTPUT_LOCATION))
+
+    minDist_min_AA_avarage = minDist_min_AA_sum / NUM_TESTS
+    full_easy_min_AA_sum_avarage = full_easy_min_AA_sum / NUM_TESTS
+
+    metrics = open("metrics.txt", "w")
+    metrics.write("AVERAGE_MIN_EASY_MIN_AA_KB {}\n".format(minDist_min_AA_avarage))
+    metrics.write("AVERAGE_FULL_EASY_MIN_AA_KB {}".format(full_easy_min_AA_sum_avarage))
+    metrics.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('--device', type=str,
+                        help='Run this script on a specific device',
+                        default=None)
+    args = parser.parse_args()
+    main(args)
