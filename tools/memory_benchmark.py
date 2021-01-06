57eb119f489bc06be9639d7b2a2d6fe91ea0660a
Igor Ianishevskyi <i.ianishevskyi@eyeo.com>
2021-01-06 11:29:02 +0100

    Noissue: Added support running on Windows for memory_benchmark.py
    * unified constructing paths by using `os.path.join`
    * using `shutils` and `os.*` instead direct call to shell commands
    * code style improvements
    * extraction of a path constant

--- tools/memory_benchmark.py
+++ tools/memory_benchmark.py
@@ -15,17 +15,20 @@ import sys
 import subprocess
 import os
 import time
+import shutil
 
 memory_benchmark_adblockwebview_test = ["MemoryBenchmark_full_easy",
     "MemoryBenchmark_minDist"]
 memory_benchmark_system_webview_test = "SystemWebViewBenchmark"
 
 TEST_PACKAGE = "org.adblockplus.libadblockplus.android.webview.test"
-CSV_FOLDER   = "adblock-android-webview/build/outputs/memory_benchmark"
+CSV_FOLDER = os.path.join("adblock-android-webview", "build", "outputs", "memory_benchmark")
+DEVICE_BENCHMARK_CSV = "/storage/emulated/0/Download/memory_benchmark.csv"
 
-NUM_TESTS    = 10
+NUM_TESTS = 10
 COOL_OFF_LABEL = "5 seconds cool off"
 TOTAL_ANDROID_STUDIO_COLUMN = 2
+FILE_METRICS = "metrics.txt"
 
 # this maps internal classpath of test to viewer name
 # ultimately its what is shown to observer
@@ -41,15 +44,18 @@ testname_to_metrics = {
     "SystemWebViewBenchmark"                : "SYSTEM_WEBVIEW_KB"
 }
 
+
 def map_to_metrics_name(testname, metric):
-    assert(testname in testname_to_metrics.keys()), "invalid test name, \
+    assert (testname in testname_to_metrics.keys()), "invalid test name, \
     please corresponding 'show' value on testname_to_metrics variable"
     return metric.upper() + "_" + testname_to_metrics[testname]
+
 
 def exit_with(message):
     print(message)
     print("exiting ...")
     sys.exit(1)
+
 
 def get_if_fnull():
     fnull = None
@@ -57,15 +63,16 @@ def get_if_fnull():
         fnull = open(os.devnull, "w")
     return fnull
 
-def run_command(command, exitOnError=True):
+
+def run_command(command, exit_on_error=True):
     fnull = get_if_fnull()
-    child = subprocess.Popen(command, shell=True, close_fds=True, stdout=fnull)
-    child.communicate()
+    child = subprocess.run(command, shell=True, stdout=fnull)
     if child.returncode != 0:
-        if exitOnError:
+        if exit_on_error:
             exit_with(command + " failed!")
         else:
             print(command + " failed!")
+
 
 def get_total_memory(filepath):
     with open(filepath, mode='r') as csv_file:
@@ -74,31 +81,34 @@ def get_total_memory(filepath):
             if row[0] == COOL_OFF_LABEL:
                 return int(row[TOTAL_ANDROID_STUDIO_COLUMN])
 
-    assert(False), "could not find cool off row"
+    assert False, "could not find cool off row"
+
 
 def launch_test_and_collect(adb_device, testname, output):
     print("cleaning previous file ...")
-    run_command("{} shell rm /storage/emulated/0/Download/memory_benchmark.csv"
-            .format(adb_device), False)
+    run_command("{} shell rm {}"
+                .format(adb_device, DEVICE_BENCHMARK_CSV), False)
 
     print("Launching {} ... ".format(testname))
     classpath = TEST_PACKAGE + "." + testname
     run_command("{} shell am instrument -w -r -e debug false -e class {} -e \
             annotation androidx.test.filters.LargeTest {}/androidx.test.runner.AndroidJUnitRunner"
-            .format(adb_device, classpath, TEST_PACKAGE))
+                .format(adb_device, classpath, TEST_PACKAGE))
     time.sleep(1)
     print("Test done pulling results ...")
-    run_command("{} pull /storage/emulated/0/Download/memory_benchmark.csv {}"
-            .format(adb_device, output))
+    run_command("{} pull {} {}"
+                .format(adb_device, DEVICE_BENCHMARK_CSV, output))
 
-def launch_tests_and_collect(NUM_TESTS, adb_device, testname):
+
+def launch_tests_and_collect(NUM_TESTS, adb_device, test_name):
     print("launch_tests_and_collect")
     for i in range(0, NUM_TESTS):
-        output = CSV_FOLDER + "/" + testname + "_" + str(i) + ".csv"
-        launch_test_and_collect(adb_device, testname, output)
+        output = CSV_FOLDER + "/" + test_name + "_" + str(i) + ".csv"
+        launch_test_and_collect(adb_device, test_name, output)
+
 
 def calc_stats(test, num_tests):
-    print ("calculating stats ...")
+    print("calculating stats ...")
     memories = []
     for i in range(0, num_tests):
         filename = CSV_FOLDER + "/" + test + "_" + str(i) + ".csv"
@@ -112,13 +122,15 @@ def calc_stats(test, num_tests):
     _min    = int(min(memories))
     return _max, average, median, _min
 
+
 def save_logcat(adb_device):
     run_command("{} logcat -d > {}/logcat.log".format(adb_device, CSV_FOLDER))
     run_command("{} logcat -d -b main > {}/logcat_main.log".format(adb_device, CSV_FOLDER))
     run_command("{} logcat -d -b crash > {}/logcat_crash.log".format(adb_device, CSV_FOLDER))
 
+
 def store_metrics_file(test, max, average, median, min):
-    metrics = open("metrics.txt", "a")
+    metrics = open(FILE_METRICS, "a")
 
     min_name     = map_to_metrics_name(test, "min")
     max_name     = map_to_metrics_name(test, "max")
@@ -131,6 +143,7 @@ def store_metrics_file(test, max, average, median, min):
     metrics.write("{} {} \n".format(average_name, average))
 
     metrics.close()
+
 
 def main(args):
     adb_device = "adb"
@@ -153,26 +166,28 @@ def main(args):
 
     print("Running memory benchmark tests...")
 
-    run_command("rm -f {}/*".format(CSV_FOLDER))
-    run_command("mkdir -p {}".format(CSV_FOLDER))
-    run_command("rm metrics.txt", False)
+    shutil.rmtree(CSV_FOLDER, ignore_errors=True)
+    os.makedirs(CSV_FOLDER)
+    if os.path.exists(FILE_METRICS):
+        os.remove(FILE_METRICS)
 
     # benchmark with adblockwebview
     for test in memory_benchmark_adblockwebview_test:
         for aa_subscription_list in ["_full_AA", "_min_AA"]:
             for aa_off_on in ["", "_off"]:
-                testname = test + aa_subscription_list + aa_off_on
-                launch_tests_and_collect(NUM_TESTS, adb_device, testname)
-                max, average, median, min = calc_stats(testname, NUM_TESTS)
-                store_metrics_file(testname, max, average, median, min)
+                test_name = test + aa_subscription_list + aa_off_on
+                launch_tests_and_collect(NUM_TESTS, adb_device, test_name)
+                max, average, median, min = calc_stats(test_name, NUM_TESTS)
+                store_metrics_file(test_name, max, average, median, min)
 
     # test with system webview
-    testname = memory_benchmark_system_webview_test
-    launch_tests_and_collect(NUM_TESTS, adb_device, testname)
-    max, average, median, min = calc_stats(testname, NUM_TESTS)
-    store_metrics_file(testname, max, average, median, min)
+    test_name = memory_benchmark_system_webview_test
+    launch_tests_and_collect(NUM_TESTS, adb_device, test_name)
+    max, average, median, min = calc_stats(test_name, NUM_TESTS)
+    store_metrics_file(test_name, max, average, median, min)
 
     save_logcat(adb_device)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
