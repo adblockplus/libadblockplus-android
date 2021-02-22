22bd74394e50f3c1ce95f52ba3e8705b7a0b5f2c
Igor Ianishevskyi <i.ianishevskyi@eyeo.com>
2021-02-23 00:01:14 +0100

    Noissue: storage_benchmark.py -- no temp files, compressed sizes, kotlin
    Contains several improvements over the storage_benchmark.py script
    
    * No unpacking of APK needed anymore, zip file stats is called instead
    * Added reporting sizes of compressed artifacts (from zip)
    * Added measuring of `kotlin` folder when Kotlin framework is added; it contains basic kotlin classes and extensions
    * Added overall APK size (webviewapp)
    * Minor improvements, like closing zip file afterwards

--- tools/storage_benchmark.py
+++ tools/storage_benchmark.py
@@ -6,35 +6,71 @@ from zipfile import ZipFile
 def main():
     file_path = "adblock-android-webviewapp/build/outputs/apk/release/adblock-android-webviewapp-release-unsigned.apk"
 
-    with ZipFile(file_path, 'r') as zip_obj:
-        list_of_file_names = zip_obj.namelist()
-        for file_name in list_of_file_names:
-            if file_name.endswith('.so'):
-                zip_obj.extract(file_name, 'temp')
-            elif file_name.endswith('.dex'):
-                zip_obj.extract(file_name, 'temp')
+    so_arm64_v8a_path = "lib/arm64-v8a/libadblockplus-jni.so"
+    so_armeabi_v7a_path = "lib/armeabi-v7a/libadblockplus-jni.so"
+    so_x86_path = "lib/x86/libadblockplus-jni.so"
+    so_x86_64_path = "lib/x86_64/libadblockplus-jni.so"
+    dex_path = "classes.dex"
+    # measures size of the `kotlin` folder in the apk; it contains basic kotlin classes and extensions
+    kotlin_path = "kotlin/"
 
-    so_arm64_v8a_path = "temp/lib/arm64-v8a/libadblockplus-jni.so"
-    arm64_v8a_storage = Path(so_arm64_v8a_path).stat().st_size / 1024
     storage_metrics = open("metrics.txt", "a")
-    storage_metrics.write("ARM64_V8A_KB {}\n".format(arm64_v8a_storage))
 
-    so_armeabi_v7a_path = "temp/lib/armeabi-v7a/libadblockplus-jni.so"
-    armeabi_v7a_storage = Path(so_armeabi_v7a_path).stat().st_size / 1024
-    storage_metrics.write("ARMEABI_V7A_KB {}\n".format(armeabi_v7a_storage))
+    kotlin_folder_size = 0
+    kotlin_folder_compressed_size = 0
 
-    so_x86_path = "temp/lib/x86/libadblockplus-jni.so"
-    x86_storage = Path(so_x86_path).stat().st_size / 1024
-    storage_metrics.write("X86_KB {}\n".format(x86_storage))
+    with ZipFile(file_path, 'r') as zip_obj:
+        list_of_file_names = zip_obj.infolist()
+        for file_info in list_of_file_names:
+            # extract to vars only once
+            filesize_kb = round(file_info.file_size / 1024)
+            filesize_compressed_kb = round(file_info.compress_size / 1024)
+            # ---------------
+            # -- arm64_v8a --
+            # ---------------
+            if file_info.filename == so_arm64_v8a_path:
+                storage_metrics.write("ARM64_V8A_KB {}\n".format(filesize_kb))
+                storage_metrics.write("ARM64_V8A_COMPRESSED_KB {}\n".format(filesize_compressed_kb))
+            # ---------------
+            # - armeabi_v7a -
+            # ---------------
+            elif file_info.filename == so_armeabi_v7a_path:
+                storage_metrics.write("ARMEABI_V7A_KB {}\n".format(filesize_kb))
+                storage_metrics.write("ARMEABI_V7A_COMPRESSED_KB {}\n".format(filesize_compressed_kb))
+            # ---------------
+            # ----- x86 -----
+            # ---------------
+            elif file_info.filename == so_x86_path:
+                storage_metrics.write("X86_KB {}\n".format(filesize_kb))
+                storage_metrics.write("X86_KB_COMPRESSED_KB {}\n".format(filesize_compressed_kb))
+            # ---------------
+            # --- x86_64 ----
+            # ---------------
+            elif file_info.filename == so_x86_64_path:
+                storage_metrics.write("X86_64_KB {}\n".format(filesize_kb))
+                storage_metrics.write("X86_64_COMPRESSED_KB {}\n".format(filesize_compressed_kb))
+            # ---------------
+            # - classes.dex -
+            # ---------------
+            elif file_info.filename == dex_path:
+                storage_metrics.write("CLASSES.DEX_KB {}\n".format(filesize_kb))
+                storage_metrics.write("CLASSES.DEX_COMPRESSED_KB {}\n".format(filesize_compressed_kb))
+            # ---------------
+            # --- Kotlin ----
+            # ---------------
+            elif file_info.filename.startswith(kotlin_path):
+                kotlin_folder_size += filesize_kb
+                kotlin_folder_compressed_size += filesize_compressed_kb
+            # ---------------
 
+    zip_obj.close()
 
-    so_x86_64_path = "temp/lib/x86_64/libadblockplus-jni.so"
-    x86_64_storage = Path(so_x86_64_path).stat().st_size / 1024
-    storage_metrics.write("X86_64_KB {}\n".format(x86_64_storage))
+    # write kotlin when calculated
+    storage_metrics.write("KOTLIN_FOLDER_KB {}\n".format(kotlin_folder_size))
+    storage_metrics.write("KOTLIN_FOLDER_COMPRESSED_KB {}\n".format(kotlin_folder_compressed_size))
 
-    dex_path = "temp/classes.dex"
-    dex_storage = Path(dex_path).stat().st_size / 1024
-    storage_metrics.write("CLASSES.DEX_KB {}\n".format(dex_storage))
+    # nice to have total webviewapp APK size in KB
+    storage_metrics.write("WEBAPP_APK_SIZE_KB {}\n".format(round(Path(file_path).stat().st_size / 1024)))
 
     storage_metrics.close()
 
